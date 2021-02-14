8448cb02cf6a8333bba91a4ff0c9ff4f59027522
Milad <nazarmilad@gmail.com>
2021-02-14 13:25:54 -0500

    Second Update

--- Starter-Code/wallet.py
+++ Starter-Code/wallet.py
@@ -2,16 +2,23 @@ import subprocess
 import json
 import bit
 import web3
+from web3 import Web3
+from dotenv import load_dotenv
+from web3.middleware import geth_poa_middleware
+#from eth_account import Account
 
 import os
 import constant
 
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
+#accounts = Account.from_key(os.getenv("0xe631cb477215f7510c5f5dc8bc0b32ee9c8b1c43dd34b44607b6037d388c93d3"))
 mnemonic = os.getenv("MNEMONIC", "donor episode angle divide modify vendor crunch argue vacuum poverty nature balance")
 
 def derive_wallets(mnemonic):
     output_json = dict()
-    command = ["/Users/miladnazar/Desktop/Homework/Unit19Homework/Starter-Code/hd-wallet-derive/hd-wallet-derive.php", "-g", "--mnemonic='"+mnemonic+"'", "--cols=path,address,privkey,pubkey", "--format=json", "--coin=eth", "--numderive=3"]
+    command = ["/Users/miladnazar/Desktop/Homework/Unit19Homework/Unit19Homework/Starter-Code/hd-wallet-derive/hd-wallet-derive.php", "-g", "--mnemonic='"+mnemonic+"'", "--cols=path,address,privkey,pubkey", "--format=json", "--coin=eth", "--numderive=3"]
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
     p_status = p.wait()
@@ -19,7 +26,7 @@ def derive_wallets(mnemonic):
 
     output_json['eth'] = command_out
 
-    command = ["/Users/miladnazar/Desktop/Homework/Unit19Homework/Starter-Code/hd-wallet-derive/hd-wallet-derive.php", "-g", "--mnemonic='"+mnemonic+"'", "--cols=path,address,privkey,pubkey", "--format=json", "--coin=btc", "--numderive=3"]
+    command = ["/Users/miladnazar/Desktop/Homework/Unit19Homework/Unit19Homework/Starter-Code/hd-wallet-derive/hd-wallet-derive.php", "-g", "--mnemonic='"+mnemonic+"'", "--cols=path,address,privkey,pubkey", "--format=json", "--coin=btc", "--numderive=3"]
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
     p_status = p.wait()
@@ -27,7 +34,7 @@ def derive_wallets(mnemonic):
 
     output_json['btc'] = command_out
 
-    command = ["/Users/miladnazar/Desktop/Homework/Unit19Homework/Starter-Code/hd-wallet-derive/hd-wallet-derive.php", "-g", "--mnemonic='"+mnemonic+"'", "--cols=path,address,privkey,pubkey", "--format=json", "--coin=btc-test", "--numderive=3"]
+    command = ["/Users/miladnazar/Desktop/Homework/Unit19Homework/Unit19Homework/Starter-Code/hd-wallet-derive/hd-wallet-derive.php", "-g", "--mnemonic='"+mnemonic+"'", "--cols=path,address,privkey,pubkey", "--format=json", "--coin=btc-test", "--numderive=3"]
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
     p_status = p.wait()
@@ -37,17 +44,36 @@ def derive_wallets(mnemonic):
 
     return output_json
 
+
 def priv_key_to_account(privkey, coin):
     if coin == constant.BTCTEST:
-        return bit.PrivateKeyTestNet(privkey)
+        return bit.PrivateKeyTestnet(privkey)
     
     elif coin == constant.ETH:
         return web3.eth.accounts.privateKeyToAccount(privkey)
 
+def create_tx(account, recipient, amount):
+    gasEstimate = w3.eth.estimateGas(
+        {"from": account.address, "to": recipient, "value": amount}
+    )
+    return {
+        "from": account.address,
+        "to": recipient,
+        "value": amount,
+        "gasPrice": w3.eth.gasPrice,
+        "gas": gasEstimate,
+        "nonce": w3.eth.getTransactionCount(account.address),
+    }
+def send_tx(account, recipient, amount):
+    tx = create_raw_tx(account, recipient, amount)
+    signed_tx = account.sign_transaction(tx)
+    result = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    print(result.hex())
+    return result.hex()
 
 
 if __name__ == "__main__":
     coins = derive_wallets(mnemonic)
     print(coins)
     print(coins['eth'][0]['privkey'])
-    print(priv_key_to_account('0xe4be1f8b03f60e7563fa45a52025fb117aff24a5ec1b9b63d16b0fc88fa0645a', 'eth'))
+
