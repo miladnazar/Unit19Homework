fb9cf6136fde08a6a5635241985f64dc8840c4ea
Milad <nazarmilad@gmail.com>
2021-02-16 21:50:12 -0500

    New Uploads

--- Starter-Code/wallet.py
+++ Starter-Code/wallet.py
@@ -16,6 +16,9 @@ w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:8545'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 private_key = os.getenv("PRIVATE_KEY")
+GANACHE_account_one = os.getenv("GANACHE_account_one")
+GANACHE_key_one = os.getenv("GANACHE_key_one")
+GANACHE_account_two = os.getenv("GANACHE_account_two")
 
 #accounts = Account.from_key(os.getenv("0xe631cb477215f7510c5f5dc8bc0b32ee9c8b1c43dd34b44607b6037d388c93d3"))
 mnemonic = os.getenv("MNEMONIC", "donor episode angle divide modify vendor crunch argue vacuum poverty nature balance")
@@ -60,23 +63,27 @@ def create_tx(coin, account, recipient, amount):
     
     if coin == constant.BTCTEST:
         return account.create_transaction([(str(recipient),amount,'btc')])
-        #gasEstimate = w3.eth.estimateGas({"from": account.address, "to": recipient, "value": amount}
-    #)
-    return {
-        "from": account.address,
-        "to": recipient,
-        "value": amount,
-        "gasPrice": w3.eth.gasPrice,
-        "gas": gasEstimate,
-        "nonce": w3.eth.getTransactionCount(account.address),
-    }
-def send_tx(coin, account, recipient, amount):
-    if coin == constant.BTCTEST:
-        print("BTC token")
+
     elif coin == constant.ETH:
+        nonce=w3.eth.getTransactionCount(account)
+            
+        tx={
+            'nonce':nonce,
+            'to':recipient,
+            'value':w3.toWei(amount,'ether'),
+            'gas':200000,
+            'gasPrice':w3.toWei('50','gwei')
+        }
+        return tx
+
+def send_tx(coin, account, recipient, amount, eth_Priv_Key):
+
+    if coin == constant.ETH:
         tx = create_tx('eth', account, recipient, amount)
-        signed_tx = account.sign_transaction(tx)
-        result = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        signed=w3.eth.account.signTransaction(tx,eth_Priv_Key)
+        tx_hash=w3.eth.sendRawTransaction(signed.rawTransaction)
+        return w3.toHex(tx_hash)
+
     elif coin == constant.BTC_TEST:
         #print("BTC_TEST token")
         tx_hash=create_tx('btc-test',account,recipient,amount)
@@ -92,8 +99,18 @@ if __name__ == "__main__":
     #print(tmp.address)
     #print(tmp.to_wif())
 
-    account=priv_key_to_account('btc-test', 'cSS7cx4X6QKULydXKZ4C6jhQxZLnF1NVc7WMTMWme94nsVtn3sVh')
-    create_tx('btc-test', account, coins['btc-test'][0]['address'], 0.00008)
+    #account=priv_key_to_account('btc-test', 'cSS7cx4X6QKULydXKZ4C6jhQxZLnF1NVc7WMTMWme94nsVtn3sVh')
+    #create_tx('btc-test', account, coins['btc-test'][0]['address'], 0.00008)
 
-    output = send_tx('btc-test', account, coins['btc-test'][0]['address'], 0.00008)
-    print(output)
+    #output = send_tx('btc-test', account, coins['btc-test'][0]['address'], 0.00008)
+    #print(output)
+    print(GANACHE_account_one)
+    print(private_key)
+    print(GANACHE_account_two)
+    print(GANACHE_key_one)
+    #account=priv_key_to_account('eth', os.getenv('GANACHE_key_one'))
+    output = send_tx('eth', GANACHE_account_one, GANACHE_account_two, 5, GANACHE_key_one)
+    print(f"Transaction Hash: {output}")
+
+
+
